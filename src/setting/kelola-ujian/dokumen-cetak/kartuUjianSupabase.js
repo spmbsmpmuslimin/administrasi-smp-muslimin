fd212d54126adc765ae66f9d285b59ee8e01f597
Joe <spmbsmpmuslimin@gmail.com>
2026-09-18 06:31:04 +0700

    update terbaru

--- src/setting/kelola-ujian/dokumen-cetak/kartuUjianSupabase.js
+++ src/setting/kelola-ujian/dokumen-cetak/kartuUjianSupabase.js
@@ -2,7 +2,7 @@
 // Layer data untuk sub-fitur "Kartu Ujian" (2 template: Peserta & Pengawas).
 //
 // Data mengalir dari 2 sub-fitur lain yang sudah jalan:
-// - Kartu Peserta  <- Pembagian Ruangan (tabel peserta_ujian)
+// - Kartu Peserta  <- Pembagian Ruangan (Tabel peserta_ujian)
 // - Kartu Pengawas <- Jadwal & Pembagian Ruangan (tabel jadwal_ujian + pengawas_ujian,
 //   lewat jadwalPengawasSupabase.js -- SENGAJA reuse fungsi yang sama biar
 //   nggak ada 2 cara beda buat ambil data yang identik)
